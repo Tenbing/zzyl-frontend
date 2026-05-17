bb00597c409ac518c04b9f246585ae4133478f9b
Tenbing <hetengbin@163.com>
2026-05-17 19:33:49 +0800

    day09: 1. 项目打包部署;

--- src/api/system/user.js
+++ src/api/system/user.js
@@ -133,3 +133,11 @@ export function deptTreeSelect() {
     method: 'get'
   })
 }
+
+// 根据部门编号，查询员工列表
+export function getAllUserList(data) {
+  return request({
+    url: '/system/user/listByDeptId?deptId=' + data.deptId,
+    method: 'get',
+  });
+}
